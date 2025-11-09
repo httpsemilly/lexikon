d0c1a0667c79839e6f9ed6a61cb7584014cc3770
Emilly Cavalcante <emilly.menezescs@gmail.com>
2025-11-09 17:52:17 -0300

    docs: add Java calculator example for lexer testing

---
+++ examples/Calculator.java
@@ -0,0 +1,12 @@
+// Simple Addition Example
+public class Calculator {
+    
+    public static void main(String[] args) {
+        // Add two numbers
+        int a = 10;
+        int b = 20;
+        int sum = a + b;
+        
+        System.out.println(a + " + " + b + " = " + sum);
+    }
+}
